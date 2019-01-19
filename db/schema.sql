6a7b5b6f46896aeb99d8bd10a18d2280565e5d09
Bhumi Shah <bshah11@outlook.com>
2019-01-19 13:37:19 -0700

    added sql schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE food_db;
+USE food_db;
+CREATE TABLE ingredients (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  name VARCHAR(250) NOT NULL,
+  category VARCHAR(50) NOT NULL,
+  PRIMARY KEY ( id ) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,49 @@
+-- vegetable seeding 
+INSERT INTO ingredients (name, category) VALUES ("Articichoke", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Argula", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Asparagus", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Beans", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Beets", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Broccoli", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Carrot", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Cabbage", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Celery", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Corn", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Cucumber", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Eggplant", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Ginger", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Garlic", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Onions", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Pepper", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Radish", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Tomatoes", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Zuchini", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Potatoes", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Squash", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Lettuce", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Basil", "Vegetable");
+INSERT INTO ingredients (name, category) VALUES ("Parsley", "Vegetable");
+-- protien seeding
+INSERT INTO ingredients (name, category) VALUES ("Chicken", "Protien");
+INSERT INTO ingredients (name, category) VALUES ("Pork", "Protien");
+INSERT INTO ingredients (name, category) VALUES ("Beef", "Protien");
+INSERT INTO ingredients (name, category) VALUES ("Fish", "Protien");
+INSERT INTO ingredients (name, category) VALUES ("Tofu", "Protien");
+-- fruit seeding
+INSERT INTO ingredients (name, category) VALUES ("Apple", "Fruit");
+INSERT INTO ingredients (name, category) VALUES ("Banana", "Fruit");
+INSERT INTO ingredients (name, category) VALUES ("Strawberry", "Fruit");
+INSERT INTO ingredients (name, category) VALUES ("Cherry", "Fruit");
+INSERT INTO ingredients (name, category) VALUES ("Pineapple", "Fruit");
+-- Dairy seeding
+INSERT INTO ingredients (name, category) VALUES ("Milk", "Dairy");
+INSERT INTO ingredients (name, category) VALUES ("Yogurt", "Dairy");
+INSERT INTO ingredients (name, category) VALUES ("Egg", "Dairy");
+INSERT INTO ingredients (name, category) VALUES ("Cheese", "Dairy");
+INSERT INTO ingredients (name, category) VALUES ("Condense Milk", "Dairy");
+-- legumes seeding
+INSERT INTO ingredients (name, category) VALUES ("Black Beans", "Legumes");
+INSERT INTO ingredients (name, category) VALUES ("Lima Beans", "Legumes");
+INSERT INTO ingredients (name, category) VALUES ("Green Beans", "Legumes");
+INSERT INTO ingredients (name, category) VALUES ("Kidney Beans", "Legumes");
+INSERT INTO ingredients (name, category) VALUES ("Edamame", "Legumes");
