d560c998ae96206a81f72afebe6788f0e752eaf6
ashackn <ashackn@gmail.com>
2019-01-23 21:01:40 -0700

    decline

--- controllers/food_controller.js
+++ controllers/food_controller.js
@@ -1,50 +1,64 @@
 var express = require("express");
-
 var router = express.Router();
+var food = require("../models/food");
+console.log ("hello world");
 
 // router.get("/",function(req, res){
 //   res.send("hello world");
 // });
 //  Import the model (food.js) to use its database functions.
-var food = require("../models/food.js");
+
 router.get("/", function(req, res) {
-// res.redirect("/view");
+ food.all(function (data) {
+   var hbsObject = {
+     view: data
+   };
+   console.log(hbsObject);
+   res.render("index", hbsObject);
+ });
+
 });
 // // Create all our routes and set up logic within those routes where required.
 router.get("/view", function (req, res) {
- food.all(function (data) {
-    var hbsObject = {
-    view: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+food.all(function (data) {
+   var hbsObject = {
+   view: data
+   };
+   // console.log(hbsObject);
+   res.render("index", hbsObject);
+ });
 });
 
 // // Our POST request to add an ingredient to the database
 // //EXPRESS POST ROUTE
-router.post('/api/create', function (req, res) {
-  food.insert([
-    'name', "category"], 
-    [req.body.name, req.body.category],
-     function (result) {
-    res.json({id: result.insertId});
-    
-  });
+router.post('/view/create', function (req, res) {
+ // food.insert([
+ //   'name', "category"],
+ //   [req.body.name, req.body.category],
+ //    function (result) {
+ //     console.log(result);
+ //     res.redirect("/");
+ food.create(req.body.name, function(result) {
+   // wrapper for orm.js that using MySQL insert callback will return a log to console,
+   // render back to index with handle
+   // console.log(result);
+   res.redirect("/view");
+
+ });
 });
 
 // //EXPRESS PUT ROUTE
-router.put('/api/view/:id', function (req, res) {
-  var newFood =  "id =" + req.params.id;
+router.put('/view/update/:id', function (req, res) {
+ var newFood =  "id =" + req.params.id;
 
-  console.log("newFood", newFood);
+ // console.log("newFood", newFood);
 
-  food.update(
-     newFood, function (data) {
-      //  res.redirect("/");
-      res.end();
-  
-  });
+ food.update(
+    newFood, function (data) {
+     //  res.redirect("/");
+     res.end();
+
+ });
 });
 
 // Export routes for server.js to use.
