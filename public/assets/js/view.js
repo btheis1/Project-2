6418ab6bc5f105770f9e26275e7bddbaac727aa8
ashackn <ashackn@gmail.com>
2019-01-28 17:25:07 -0700

    empty

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,9 +1,7 @@
 //INITIALIZE parallax
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
 //INITIALIZE collapsable
 $(document).ready(function(){
+    $('.parallax').parallax();
     $('.collapsible').collapsible();
-  });
+     });
+
